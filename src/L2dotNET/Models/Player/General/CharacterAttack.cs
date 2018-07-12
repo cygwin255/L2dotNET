976c636a5843666a4b1ca57af310de83f080c3f6
Andrei Ledauski <andrei.ledauski@itechart-group.com>
2018-07-12 19:24:25 +0300

    Implemented basic character attack logic

---
+++ src/L2dotNET/Models/Player/General/CharacterAttack.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using L2dotNET.DataContracts.Shared.Enums;
+using L2dotNET.Network.serverpackets;
+using L2dotNET.Utility;
+using NLog.Targets;
+
+namespace L2dotNET.Models.Player.General
+{
+    public class CharacterAttack
+    {
+        public bool IsAttacking { get; private set; }
+        public bool IsCasting { get; private set; }
+
+        private readonly L2Character _character;
+        private L2Character _target;
+
+        private Task _autoAttackTask;
+
+        public CharacterAttack(L2Character character)
+        {
+            _character = character;
+        }
+
+        public async void DoAttack(L2Character target) // TODO: Add skill
+        {
+            if (!CanAttack(target) || target == _target)
+            {
+                _character.SendActionFailedAsync();
+                return;
+            }
+
+            if (IsAttacking)
+            {
+                await CancelAttack();
+            }
+
+            IsAttacking = true;
+
+            _target = target;
+
+            _autoAttackTask = Task.Factory.StartNew(PerformAutoAttack); // for bow and melee weapon
+        }
+
+        public async Task CancelAttack()
+        {
+            IsAttacking = false;
+            await _autoAttackTask;
+        }
+
+        public bool CanAttack(L2Character target = null, bool moveIfTooFar = true)
+        {
+            if (target == null)
+            {
+                target = _target;
+            }
+
+            if (target == null || target.Dead) // TODO: add check for immune/abnormal/GM
+            {
+                return false;
+            }
+
+            // TODO: Move to config
+            _character.SendMessageAsync($"[attack]distance to target is {(int)_character.CharMovement.DistanceTo(target)}");
+            if (_character.CharMovement.DistanceToSquared(target) > 100*100 * 1.05) // TODO : use Weapon range
+            {
+                if (moveIfTooFar)
+                {
+                    _character.SendMessageAsync("[attack]targer is too far, moving for it");
+                    _character.CharMovement.MoveToAndHit(target, 100);
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private async Task PerformAutoAttack()
+        {
+            // TODO: revalidate that on every attack
+            int attackSpeed = (int) (470000 / _character.CharacterStat.PAttackSpeed); // TODO: calculate real attack speed
+            bool dual = true; // is dual weapon, harcode for now
+
+            while (CanAttack() && IsAttacking)
+            {
+                Attack attackPacket = new Attack(_character, GenerateSimpleHit(dual));
+
+                if (dual)
+                {
+                    attackPacket.Hits.Add(GenerateSimpleHit(dual));
+                }
+
+                await _character.BroadcastPacketAsync(attackPacket);
+
+                await Task.Delay(dual ? attackSpeed / 2 : attackSpeed - 5);
+
+                if (!CanAttack() || !IsAttacking)
+                {
+                    break;
+                }
+
+                PerformHit(attackPacket.Hits[0]);
+
+                if (dual)
+                {
+                    await Task.Delay(attackSpeed / 2 - 5);
+
+                    if (!CanAttack() || !IsAttacking)
+                    {
+                        break;
+                    }
+
+                    PerformHit(attackPacket.Hits[1]);
+                }
+            }
+
+            IsAttacking = false;
+            _target = null;
+        }
+
+        private async void PerformHit(Hit hit)
+        {
+            if (hit.IsMiss)
+            {
+                await _character.SendPacketAsync(new SystemMessage(SystemMessageId.MissedTarget));
+
+                if (_target is L2Player)
+                {
+                    await _target.SendPacketAsync(new SystemMessage(SystemMessageId.AvoidedS1Attack)
+                        .AddName(_character));
+                }
+            }
+            else
+            {
+                _target.CharStatus.ReduceHp(hit.Damage, _character);
+
+                // start autoAttack
+
+                await _character.SendPacketAsync(new SystemMessage(SystemMessageId.YouDidS1Dmg).AddNumber(hit.Damage));
+
+                if (hit.IsCritical)
+                {
+                    await _character.SendPacketAsync(new SystemMessage(SystemMessageId.CriticalHit));
+                }
+
+                if (_target is L2Player)
+                {
+                    await _target.SendPacketAsync(new SystemMessage(SystemMessageId.C1HasReceivedS3DamageFromC2)
+                        .AddName(_target)
+                        .AddName(_character)
+                        .AddNumber(hit.Damage));
+                }
+            }
+        }
+
+        private Hit GenerateSimpleHit(bool dual)
+        {
+            int damage = RandomThreadSafe.Instance.Next(5, 20) / (dual ? 2 : 1);
+            bool isMiss = RandomThreadSafe.Instance.NextDouble() < 0.10d;
+            bool isCritical = RandomThreadSafe.Instance.NextDouble() < 0.15d;
+            int shield = 0;
+
+            if (isCritical)
+            {
+                damage *= 2;
+            }
+
+            return new Hit(_target.ObjectId, damage, isMiss, isCritical, shield, false, 0);
+        }
+    }
+}
